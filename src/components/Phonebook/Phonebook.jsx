3d57402b2111b9e506d175eaf7eb10f0fb6722d2
Stavr101 <Shishkin_SU@ukr.net>
2022-11-07 18:30:40 +0200

    finish

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,21 +1,15 @@
 import { FormAddContact } from './FormAddContact';
 import { ContactsList } from './ContactsList';
 import Filter from './Filter';
-import { useDispatch } from 'react-redux';
-import { addContact } from 'redux/contacts/userContactSlice';
-
-import { setFilter } from 'redux/filters/filtersSlice';
 
 export default function Phonebook() {
-  const dispatch = useDispatch();
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <FormAddContact onSubmit={() => dispatch(addContact)} />
+      <FormAddContact />
 
       <h2>Contacts</h2>
-      <Filter onChange={() => dispatch(setFilter)} />
+      <Filter />
       <ContactsList />
     </div>
   );
